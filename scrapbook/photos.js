81e7a1b0b62907fab41730bb828daa713fda53b2
dariakimberly4-netizen <dariakimberly4@gmail.com>
2026-08-25 08:16:04 +0800

    Add static wedding photo manifest

---
+++ scrapbook/photos.js
@@ -0,0 +1,8 @@
+// Danley & Maria static wedding scrapbook
+// Add selected photo files to this folder/repository, then list them here.
+// Example:
+// window.WEDDING_PHOTOS = [
+//   { src: "photos/ceremony-01.jpg", caption: "Ceremony" },
+//   { src: "photos/reception-01.jpg", caption: "Reception" }
+// ];
+window.WEDDING_PHOTOS = [];
